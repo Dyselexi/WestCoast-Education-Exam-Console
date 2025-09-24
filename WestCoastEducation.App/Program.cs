cb6da80984f9561061da58810f1c8520896a9a73
JohanWidell <johan.widell@hotmail.se>
2025-09-24 12:13:15 +0200

    added Classes

--- WestCoastEducation.App/Program.cs
+++ WestCoastEducation.App/Program.cs
@@ -1,9 +1,16 @@
 ﻿namespace WestCoastEducation.App;
+using WestCoastEducation.Domain.Models;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Students elev = new Students ("Johan");
+        Teacher lärare = new Teacher ("Marie");
+        
+        
+        Console.WriteLine(elev);
+        Console.WriteLine("===========");
+        Console.WriteLine(lärare);
     }
 }
