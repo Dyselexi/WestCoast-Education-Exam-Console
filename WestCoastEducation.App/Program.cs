a86488bc254cea40f419559ecc997f9a7961ef9e
JohanWidell <johan.widell@hotmail.se>
2025-09-24 14:25:17 +0200

    Added info

--- WestCoastEducation.App/Program.cs
+++ WestCoastEducation.App/Program.cs
@@ -1,32 +1,37 @@
 ﻿namespace WestCoastEducation.App;
 
 using System.ComponentModel;
+using System.Text.Json.Serialization;
 using WestCoastEducation.Domain.Models;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Student elev = new Student("Johan");
-        Teacher lärare = new Teacher("Marie");
-        EducationalLeader administratör = new EducationalLeader("Gittan");
-        Admin adm = new Admin("Chrille");
+        Student elev = new Student("Johan", "Widell", 0708717599, 19950301, "Mariedalsvägen", 21745, "Malmö");
+        // Teacher lärare = new Teacher("Marie");
+        // EducationalLeader administratör = new EducationalLeader("Gittan");
+        // Admin adm = new Admin("Chrille");
+
 
         Console.WriteLine(elev);
-        Console.WriteLine("");
-        Console.WriteLine("===========");
-        Console.WriteLine("");
-        Console.WriteLine(lärare);
-        Console.WriteLine("");
-        Console.WriteLine("===========");
-        Console.WriteLine("");
-        Console.WriteLine(administratör);
-        Console.WriteLine("");
-        Console.WriteLine("===========");
-        Console.WriteLine(adm);
-        Console.WriteLine("");
-        Console.WriteLine("===========");
-
         
+
+        // Console.WriteLine(elev);
+        // Console.WriteLine("");
+        // Console.WriteLine("===========");
+        // Console.WriteLine("");
+        // Console.WriteLine(lärare);
+        // Console.WriteLine("");
+        // Console.WriteLine("===========");
+        // Console.WriteLine("");
+        // Console.WriteLine(administratör);
+        // Console.WriteLine("");
+        // Console.WriteLine("===========");
+        // Console.WriteLine(adm);
+        // Console.WriteLine("");
+        // Console.WriteLine("===========");
+
+
     }
 }
